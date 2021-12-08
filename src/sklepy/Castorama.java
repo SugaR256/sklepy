d09386c818263f0f1c864b8d89a643c6821cec59
SugaR256 <kuba.krapiec@outlook.com>
2021-12-08 17:46:39 +0100

    add classes Castorama and LeroyMerlin

---
+++ src/sklepy/Castorama.java
@@ -0,0 +1,79 @@
+package sklepy;
+
+import java.util.Objects;
+
+public class Castorama extends SklepBudowniczy {
+
+    private boolean czySaStanowiskaSamoobslugowe;
+    private String dzialPromocyjny;
+
+    private final double rabatDzial = .20;
+    private final double rabat = .05;
+    private final double minCena = 200;
+
+
+    public Castorama(String adres, String adresWWW, boolean czySaKasySamoobslugowe, String dzialPromocyjny) {
+        super(adres, adresWWW);
+        this.czySaStanowiskaSamoobslugowe = czySaKasySamoobslugowe;
+        this.dzialPromocyjny = dzialPromocyjny;
+    }
+
+    @Override
+    public boolean czyJestOtwarty(String dzienTygodnia, int godzina) {
+        // Niedziela 10-18, reszta 6-21
+        if (Objects.equals(dzienTygodnia, DniTygodnia.NIEDZIELA)) {
+            return godzina >= 10 && godzina <= 18;
+        } else {
+            return godzina >= 6 && godzina <= 21;
+        }
+    }
+
+    @Override
+    public double sprzedajProdukt(Produkt produkt, int ilosc) {
+        int dotychczasowaIlosc = sprawdzDostepnoscProduktu(produkt);
+
+        // Nie mozemy sprzedac wiecej niz dostepna ilosc na sklepie
+        if (ilosc > dotychczasowaIlosc) {
+            ilosc = dotychczasowaIlosc;
+        }
+
+        // Zmniejszamy ilosc produktow na magazynie
+        aktualizujIloscProduktow(produkt, -ilosc);
+
+        double suma = ilosc * produkt.getCena();
+
+
+        // obowiazuje rabat na konkretny dzial
+        if (Objects.equals(dzialProduktu.get(produkt), dzialPromocyjny))
+            suma -= suma * rabatDzial;
+
+
+        // Rabaty sie sumuja
+        // Obowiazuje rabat powyzej minimalnej ceny
+        if (suma >= minCena) {
+            suma -= suma * rabat;
+        }
+
+        return suma;
+    }
+
+    @Override
+    // raczej nie o to chodzilo, ale mysle ze git jest
+    public void wystawka() {
+        System.out.println("W dniu dzisiejszym dział " + dzialPromocyjny + " podlega promocji - " + rabatDzial * 100 + "%");
+    }
+
+    // ------------------- GETTERS & SETTERS -----------------------------
+
+    public boolean getCzySaKasySamoobslugowe() {
+        return czySaStanowiskaSamoobslugowe;
+    }
+
+    public void setCzySaKasySamoobslugowe(boolean czySaKasySamoobslugowe) {
+        this.czySaStanowiskaSamoobslugowe = czySaKasySamoobslugowe;
+    }
+
+    public void zmienDzialPromocyjny(String dzialPromocyjny) {
+        this.dzialPromocyjny = dzialPromocyjny;
+    }
+}
---
+++ src/sklepy/LeroyMerlin.java
@@ -0,0 +1,82 @@
+package sklepy;
+
+import java.util.Objects;
+
+public class LeroyMerlin extends SklepBudowniczy {
+
+    private boolean czySaStanowiskaSamoobslugowe;
+    private String dzialPromocyjny;
+
+    private final double rabatDzial = .25;
+    private final double rabat = .08;
+    private final double minCena = 300;
+
+
+    public LeroyMerlin(String adres, String adresWWW, boolean czySaKasySamoobslugowe, String dzialPromocyjny) {
+        super(adres, adresWWW);
+        this.czySaStanowiskaSamoobslugowe = czySaKasySamoobslugowe;
+        this.dzialPromocyjny = dzialPromocyjny;
+    }
+
+    @Override
+    public void wystawka() {
+        System.out.println("W dniu dzisiejszym dział " + dzialPromocyjny + " podlega promocji - " + rabatDzial * 100 + "%");
+    }
+
+    @Override
+    public boolean czyJestOtwarty(String dzienTygodnia, int godzina) {
+
+        // Niedziela 10-20, reszta 7-21
+        if (Objects.equals(dzienTygodnia, DniTygodnia.NIEDZIELA)) {
+            return godzina >= 10 && godzina <= 20;
+        } else {
+            return godzina >= 7 && godzina <= 21;
+        }
+    }
+
+    @Override
+    public double sprzedajProdukt(Produkt produkt, int ilosc) {
+
+        int dotychczasowaIlosc = sprawdzDostepnoscProduktu(produkt);
+
+        // Nie mozemy sprzedac wiecej niz dostepna ilosc na sklepie
+        if (ilosc > dotychczasowaIlosc)
+            ilosc = dotychczasowaIlosc;
+
+        // Zmniejszamy ilosc produktow na magazynie
+        aktualizujIloscProduktow(produkt, -ilosc);
+
+        double suma = ilosc * produkt.getCena();
+
+
+        // sprawdza czy obowiazuje rabat dzialowy
+        if (dzialProduktu.get(produkt) == dzialPromocyjny) {
+            suma -= suma * rabatDzial;
+        }
+
+        // Rabaty sie sumuja
+        // Obowiazuje rabat powyzej minimalnej ceny
+        if (suma >= minCena) {
+            suma -= suma * rabat;
+        }
+
+        return suma;
+    }
+
+
+    // ------------------- GETTERS & SETTERS -----------------------------
+
+    public boolean getCzySaKasySamoobslugowe() {
+        return czySaStanowiskaSamoobslugowe;
+    }
+
+    public void setCzySaKasySamoobslugowe(boolean czySaKasySamoobslugowe) {
+        this.czySaStanowiskaSamoobslugowe = czySaKasySamoobslugowe;
+    }
+
+    public void zmienDzialPromocyjny(String dzialPromocyjny) {
+        this.dzialPromocyjny = dzialPromocyjny;
+    }
+
+
+}
